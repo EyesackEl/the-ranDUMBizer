1a4076d36b25342afcc127c4c4cb3fc25e8b14fe
Matthew Davis <matthewdavis1372@yahoo.com>
2022-09-08 22:22:54 -0400

    one more email

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,7 +21,7 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+    login(username: String!, password: String!): Auth
   }
 `;
 
