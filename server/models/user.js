cab421a69cea0172ce43dc9f2b7b0b1b28f8c48e
Adam Parent <102196654+arparent@users.noreply.github.com>
2022-09-08 19:20:32 -0700

    Removed email

--- server/models/user.js
+++ server/models/user.js
@@ -7,11 +7,6 @@ const userSchema = new Schema({
         required: true,
         trim: true,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
     password: {
         type: String,
         required: true,
